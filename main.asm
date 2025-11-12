484f1018880351c00851a965c45b8b6ace2be098
Anderrh <andercode@protonmail.com>
2025-11-11 21:45:05 -0800

    mem copy func

--- main.asm
+++ main.asm
@@ -21,40 +21,19 @@ WaitVBlank:
     ld de, Tiles
     ld hl, $9000
     ld bc, TilesEnd - Tiles
-CopyTiles:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyTiles
+		call Memcopy
 
     ; Copy the tilemap
     ld de, Tilemap
     ld hl, $9800
     ld bc, TilemapEnd - Tilemap
-CopyTilemap:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyTilemap
+		call Memcopy
 
     ; Copy the paddle tile
     ld de, Paddle
     ld hl, $8000
     ld bc, PaddleEnd - Paddle
-CopyPaddle:
-    ld a, [de]
-    ld [hli], a
-    inc de
-    dec bc
-    ld a, b
-    or a, c
-    jp nz, CopyPaddle
+		call Memcopy
 
     ld a, 0
     ld b, 160
@@ -114,6 +93,20 @@ WaitVBlank2:
     ld [_OAMRAM + 1], a
     jp Main
 
+; Copy bytes from one area to another.
+; @param de: Source
+; @param hl: Destination
+; @param bc: Length
+Memcopy:
+    ld a, [de]
+    ld [hli], a
+    inc de
+    dec bc
+    ld a, b
+    or a, c
+    jp nz, Memcopy
+    ret
+		
 Tiles:
 	dw `33333333
 	dw `33333333
